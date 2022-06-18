cd56043f92a67f9d11ee884a586d5ed316434197
Pranavchiku <goswami.4@iitj.ac.in>
2022-06-18 16:20:12 +0530

    Create 188.cpp

---
+++ Binary Trees/188.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+typedef struct Node{
+    int data;
+    Node* left;
+    Node* right;
+}Node;
+
+Node* createNode(int data){
+    Node *n=new(Node);
+    n->right=n->left=NULL;
+    n->data=data;
+    return n;
+}
+
+vector<int> topView(Node *root)
+{
+    queue<pair<Node*,int>> q; // for bfs
+    map<int, int>m; // height, value
+    vector<int> v;
+    if(!root) return v;
+
+    q.push({root,0});
+    while(!q.empty()){
+        Node *t=q.front().first;
+        int h=q.front().second;
+        q.pop();
+        if(m[h]==0) m[h]=t->data;
+        if(t->left) q.push({t->left,h-1});
+        if(t->right) q.push({t->right,h+1});
+    }
+    for(auto x:m){
+        v.push_back(x.second);
+    }
+    return v;
+}
