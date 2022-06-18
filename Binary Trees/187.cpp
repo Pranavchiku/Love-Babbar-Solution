b4b6f05dcc3d3eea0a77ae1b72d37da0b3e64b62
Pranavchiku <goswami.4@iitj.ac.in>
2022-06-18 15:50:03 +0530

    187

---
+++ Binary Trees/187.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+typedef struct Node{
+    int data;
+    Node* left;
+    Node* right;
+}Node;
+
+Node* createNode(int data){
+    Node *n=new(Node);
+    n->right=n->left=NULL;
+    n->data=data;
+    return n;
+}
+
+void insert(Node *root, int key){
+
+    Node* prev=NULL;
+    Node *n=createNode(key);
+    while(root!=NULL){
+        prev=root;
+        if(key==root->data) return;
+        else if(key<root->data) root=root->left;
+        else root=root->right;
+    }
+    if(key>prev->data) prev->right=n;
+    else prev->left=n;
+}
+void preOrder(Node *root, vector<int> &v, int curr, bool levels[]){
+    if(root!=NULL){
+        if(!levels[curr]){
+            v.push_back(root->data);
+            levels[curr]=1;
+        }
+        preOrder(root->left,v,curr+1,levels);
+        preOrder(root->right,v,curr+1,levels);
+    }
+}
+vector<int> leftView(Node *root)
+{
+   vector<int> v;
+   bool levels[101]={0};
+   preOrder(root,v,0,levels);
+   return v;
+}
+
+void mirrorify(Node *root, Node **mirror){
+    if(root==NULL){
+        mirror=NULL;
+        return;
+    }
+
+    *mirror=createNode(root->data);
+    mirrorify(root->left,&((*mirror)->right));
+    mirrorify(root->right,&((*mirror)->left));
+}
+
+vector<int> rightView(Node *root)
+{
+    Node *mirror=NULL;
+    mirrorify(root,&mirror);
+    vector<int> v=leftView(mirror);
+    return v;
+}
