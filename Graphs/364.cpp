cd3da5201715d0c7920081070dd69e722230ad28
Pranavchiku <goswami.4@iitj.ac.in>
2022-06-21 13:40:57 +0530

    363

---
+++ Graphs/364.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int minStepToReachTarget(vector<int>&KnightPos,vector<int>&TargetPos,int n)
+{
+    vector<vector<int>> visited;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            visited[i][j]=0;
+        }
+    }
+    
+}
